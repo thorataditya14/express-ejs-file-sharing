82e63c9056726f22e0385ce10c4b8b6221cdbbf9
Aditya Thorat <thorat.aditya@outlook.com>
2022-08-31 08:17:30 +0530

    complete express-file-sharing

---
+++ index.js
@@ -0,0 +1,61 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
+
+const bcrypt = require("bcrypt");
+
+const File = require("./models/File")
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI)
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname,
+    };
+    if ((req.body.password != null) && (req.body.password !== "")) {
+        fileData.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const file = await File.create(fileData);
+
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
+});
+
+async function handleDownload(req, res) {
+    const file = await File.findById(req.params.id);
+
+    if (file.password != null) {
+        if (req.body.password == null) {
+            res.render("password");
+            return;
+        }
+    }
+
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+        res.render("password", { error: true });
+        return;
+    }
+
+    ++file.downloadCount;
+    await file.save();
+
+    res.download(file.path, file.originalName);
+}
+
+app.route("/file/:id").get(handleDownload).post(handleDownload);
+
+app.listen(process.env.PORT);
